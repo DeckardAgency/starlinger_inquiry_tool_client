1563ef4ece29934ba991fe6dc95fdac78d2e33ab
Nikola Grdanjski <ngrdanjski88@gmail.com>
2025-04-07 20:22:41 +0200

    Add Staging API URL

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -10,27 +10,20 @@ import { environment } from '../../../environment';
 })
 export class ProductService {
   private apiUrl: string;
-  private isServer: boolean;
 
   constructor(
     private http: HttpClient,
     @Inject(PLATFORM_ID) private platformId: Object
   ) {
-    this.isServer = isPlatformServer(this.platformId);
-
-    // During server-side rendering, we need absolute URLs
-    const baseUrl = this.isServer ? environment.serverUrl : '';
-    this.apiUrl = `${baseUrl}${environment.apiPath}/products?itemsPerPage=300`;
+    // Always use the apiBaseUrl from environment
+    this.apiUrl = `${environment.apiBaseUrl}${environment.apiPath}/products`;
   }
 
   getProducts(): Observable<ProductResponse> {
-    return this.http.get<ProductResponse>(this.apiUrl);
+    return this.http.get<ProductResponse>(`${this.apiUrl}?itemsPerPage=300`);
   }
 
   getProduct(id: string): Observable<any> {
-    // Using the same pattern for the product detail endpoint
-    const baseUrl = this.isServer ? environment.serverUrl : '';
-    const productUrl = `${baseUrl}${environment.apiPath}/products/${id}`;
-    return this.http.get(productUrl);
+    return this.http.get(`${this.apiUrl}/${id}`);
   }
 }
