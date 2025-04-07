721a398f9d5239b6fda559cdd9b2d59749cd591d
Nikola Grdanjski <ngrdanjski88@gmail.com>
2025-04-07 20:08:56 +0200

    Add Staging API URL

--- environment.ts
+++ environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://phpstack-675879-5400964.cloudwaysapps.com/', // for asset URLs
+  apiBaseUrl: 'https://phpstack-675879-5400964.cloudwaysapps.com', // for asset URLs
   apiPath: '/api/v1', // for API endpoints
-  serverUrl: 'https://phpstack-675879-5400964.cloudwaysapps.com/' // for server-side rendering
+  serverUrl: 'https://phpstack-675879-5400964.cloudwaysapps.com' // for server-side rendering
 };
