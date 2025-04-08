a1345b4bd899d57895c7e1c62c31d4e2d2eed198
Nikola Grdanjski <ngrdanjski88@gmail.com>
2025-04-08 15:41:30 +0200

    Upgrade Search Component

--- src/app/layout/topbar/top-bar.component.ts
+++ src/app/layout/topbar/top-bar.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { CartService } from '../../services/cart.service';
 import { QuickCartService } from '../../services/quick-cart.service';
 import { Router } from '@angular/router';
+import {SearchComponent} from '../../components/search/search.component';
 
 @Component({
   selector: 'app-top-bar',
   standalone: true,
+  imports: [SearchComponent],
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.scss']
 })
